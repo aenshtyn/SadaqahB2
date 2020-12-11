e3b51d3e34245bd3fc85a78e124eef71550cff13
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-12-11 10:32:13 +0300

    settings update

--- sadaqah/settings.py
+++ sadaqah/settings.py
@@ -18,52 +18,8 @@ from decouple import config,Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ')0hh&qp#2#w!re(bt%t$+=3dh6r#x&ysbfrxwn2+%n5&7vb^7p'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# ALLOWED_HOSTS = []
-
-# MODE=config("MODE", default="dev")
-# SECRET_KEY = config('SECRET_KEY')
-# DEBUG = config('DEBUG', default=False, cast=bool)
-# # development
-# # if config('MODE')=="dev":
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': '',
-#     }
-    
-# }
-# # production
-# # else:
-# #    DATABASES = {
-# #        'default': dj_database_url.config(
-# #            default=config('DATABASE_URL')
-# #        )
-# #    }
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
-
 MODE=config("MODE", default="dev")
-
 SECRET_KEY = config('SECRET_KEY')
-
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
 # if config('MODE')=="dev":
